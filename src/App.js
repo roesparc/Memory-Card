dfb5fb72491642225c618ed0c3b7d80004eab1f9
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-02-24 13:14:46 -0600

    Add Game component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Game from "./components/Game";
+
 function App() {
   return (
     <div className="App">
       <header>
         <h1>LOTR Memory Card</h1>
       </header>
-      <main></main>
+      <main>
+        <Game />
+      </main>
       <footer>
         <p>
           By <a href="https://github.com/roesparc/">roesparc</a>
