4752b9bcdbfbc60a1488acc3ec6c7965af3bc5a1
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-02-23 19:22:47 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>LOTR Memory Card</h1>
       </header>
+      <main></main>
+      <footer>
+        <p>
+          By <a href="https://github.com/roesparc/">roesparc</a>
+        </p>
+        <a href="https://github.com/roesparc/">
+          <i className="fa-brands fa-github"></i>
+        </a>
+      </footer>
     </div>
   );
 }
