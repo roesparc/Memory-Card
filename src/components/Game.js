4f135922503a256741bf93908a39b7b2eb88e968
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-02-24 18:59:46 -0600

    Display shuffled game cards

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import cards from "../assets/cardData";
 
 const Game = () => {
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [randomCards, setRandomCards] = useState(cards);
+
+  useEffect(() => {
+    const shuffled = cards.slice();
+
+    for (let i = shuffled.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+
+    setRandomCards(shuffled);
+  }, [currentScore]);
 
   return (
     <div className="game">
@@ -15,6 +28,15 @@ const Game = () => {
           <p>Best Score</p>
           <p>{bestScore}</p>
         </div>
+      </div>
+
+      <div className="cards">
+        {randomCards.map((card) => (
+          <div key={card.id} className="card">
+            <img src={card.image} alt={card.name} />
+            <p>{card.name}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
