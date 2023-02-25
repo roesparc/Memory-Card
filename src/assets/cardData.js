037afbfc1688ea2702e5231352901b2aa81ea201
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-02-24 19:02:33 -0600

    Add card data array

---
+++ src/assets/cardData.js
@@ -0,0 +1,77 @@
+import frodo from "./img/frodo.jpg";
+import sam from "./img/sam.jpg";
+import pippin from "./img/pippin.jpg";
+import merry from "./img/merry.jpg";
+import gandalf from "./img/gandalf.jpg";
+import aragorn from "./img/aragorn.jpg";
+import legolas from "./img/legolas.jpg";
+import gimli from "./img/gimli.jpg";
+import boromir from "./img/boromir.jpg";
+import sauron from "./img/sauron.jpg";
+import gollum from "./img/gollum.jpg";
+import bilbo from "./img/bilbo.jpg";
+
+const cards = [
+  {
+    id: 1,
+    name: "Frodo Baggins",
+    image: frodo,
+  },
+  {
+    id: 2,
+    name: "Samwise Gamgee",
+    image: sam,
+  },
+  {
+    id: 3,
+    name: "Peregrin Took",
+    image: pippin,
+  },
+  {
+    id: 4,
+    name: "Meriadoc Brandybuck",
+    image: merry,
+  },
+  {
+    id: 5,
+    name: "Gandalf the Grey",
+    image: gandalf,
+  },
+  {
+    id: 6,
+    name: "Aragorn II Elessar",
+    image: aragorn,
+  },
+  {
+    id: 7,
+    name: "Legolas",
+    image: legolas,
+  },
+  {
+    id: 8,
+    name: "Gimli",
+    image: gimli,
+  },
+  {
+    id: 9,
+    name: "Boromir",
+    image: boromir,
+  },
+  {
+    id: 10,
+    name: "Sauron",
+    image: sauron,
+  },
+  {
+    id: 11,
+    name: "Gollum",
+    image: gollum,
+  },
+  {
+    id: 12,
+    name: "Bilbo Baggins",
+    image: bilbo,
+  },
+];
+
+export default cards;
